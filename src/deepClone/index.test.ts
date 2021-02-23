1a2e94df3ffdb302f7e4059a78c02dbae7c993d9
hzjipeng <hzjipeng@corp.netease.com>
2021-02-23 15:14:31 +0800

    Added: 深拷贝及其单侧完善

--- src/deepClone/index.test.ts
+++ src/deepClone/index.test.ts
@@ -1,29 +1,93 @@
-import { deepClone } from './index';
+import { deepClone, isObject } from './index';
 
-expect.extend({
-  isSuccessClone(received, target) {
-    if (received === target) {
-      return {
-        message: () => '这是浅拷贝的结果',
-        pass: false,
+const isSuccessClone = (received, target) => {
+  if (isObject(received) && received === target) {
+    return {
+      message: () => '这是浅拷贝的结果',
+      pass: false,
+    }
+  }
+  const pass = JSON.stringify(received) === JSON.stringify(target);
+  if (pass) {
+    return {
+      message: () => '比对通过',
+      pass: true,
+    };
+  } else {
+    return {
+      message: () => {
+        console.log(received);
+        return '比对不通过';
+      },
+      pass: false,
+    };
+  }
+};
+
+const mapSuccessClone = (received: Map<any, any>, target: Map<any, any>) => {
+  if (received === target) {
+    return {
+      message: () => '这是浅拷贝的结果',
+      pass: false,
+    }
+  }
+  for (const item of received) {
+    if (isObject(item)) {
+      if (target.get(item)) {
+        console.warn(item);
+        return {
+          message: () => 'Map的某个为对象的key未进行深拷贝',
+          pass: false,
+        }
+      }
+    } else {
+      const res = isSuccessClone(received.get(item), target.get(item));
+      if (!res.pass) {
+        console.warn(item);
+        return res;
       }
     }
-    const pass = JSON.stringify(received) === JSON.stringify(target);
-    if (pass) {
-      return {
-        message: () => '比对通过',
-        pass: true,
-      };
-    } else {
-      return {
-        message: () => {
-          console.log(received);
-          return '比对不通过';
-        },
+  }
+  return {
+    message: () => '比对通过',
+    pass: true,
+  };
+}
+
+const setSuccessClone = (received: Set<any>, target: Set<any>) => {
+  if (received === target) {
+    return {
+      message: () => '这是浅拷贝的结果',
+      pass: false,
+    }
+  }
+  for (const item of received) {
+    if (isObject(item)) {
+      if (target.has(item)) {
+        console.warn(item);
+        return {
+          message: () => 'Set中的某项未进行深拷贝',
+          pass: false,
+        }
+      }
+    } else if (!target.has(item)) {
+      console.warn(item);
+      return  {
+        message: () => '这是浅拷贝的结果',
         pass: false,
       };
     }
   }
+  return {
+    message: () => '比对通过',
+    pass: true,
+  };
+}
+
+expect.extend({
+  isSuccessClone,
+  mapSuccessClone,
+  setSuccessClone,
 });
 
 const testCase = [
@@ -38,13 +102,50 @@ const testCase = [
       [1, 2, { name: 100 }], 
       { arr: [1, 2, 3] },
     ]
+  },
+  {
+    input: [null, true],
   }
 ];
 
 describe('深拷贝结果比较', () => {
   testCase.forEach(({ input }) => {
-    it('结果比较', () => {
+    it('普通数组、对象的嵌套结构', () => {
       expect(deepClone(input)).isSuccessClone(input);
     });
   })
+
+  it('Map结构', () => {
+    const mapDemo = new Map();
+    mapDemo.set('name', 12);
+    expect(deepClone(mapDemo)).mapSuccessClone(mapDemo);
+    const objKey = { name: 'a object in map' };
+    mapDemo.set(objKey, 'hahaha');
+    expect(deepClone(mapDemo)).mapSuccessClone(mapDemo);
+  });
+
+  it('Set结构', () => {
+    const setDemo = new Set<any>([1, 2, null]);
+    expect(deepClone(setDemo)).setSuccessClone(setDemo);
+    setDemo.add({ name: 'a object in set' });
+    expect(deepClone(setDemo)).setSuccessClone(setDemo);
+  });
+
+  it('循环引用拷贝', () => {
+    const objDemo: any = { 
+      name: 'jipeng', 
+      next: { level: 2 },
+    };
+    const tempObj: any = { level: 3 };
+    tempObj.next = tempObj;
+    objDemo.next.next = tempObj;
+    const clonedObjDemo = deepClone(objDemo);
+    expect(clonedObjDemo.next.next.next === clonedObjDemo.next.next && clonedObjDemo.next.next !== tempObj).toBe(true);
+
+    const setDemo: Set<number | any> = new Set([1,2,3]);
+    setDemo.add(setDemo);
+    const clonedSet = deepClone(setDemo);
+
+    expect(setDemo !== clonedSet && clonedSet.has(clonedSet)).toBe(true);
+  });
 });
--- src/deepClone/index.ts
+++ src/deepClone/index.ts
@@ -1,28 +1,70 @@
+const nullTag = 'null';
+const undefinedTag = 'undefined';
+const stringTag = 'string';
+const numberTag = 'number';
+const biginitTag = 'bigint';
+const booleanTag = 'boolean';
+const symbolTag = 'symbol';
+
+const objectTag = 'object';
+const arrayTag = 'array';
+const funtionTag = 'function';
+
+const setTag = 'set';
+const mapTag = 'map';
+
+export const isObject = (target) => {
+  const type = typeof target;
+  return target !== null && (type === 'object' || type === 'function');
+}
+
 const getType = input => Object.prototype.toString.call(input).slice(8, -1).toLowerCase();
 
-export const deepClone = (obj) => {
-  const type = getType(obj);
-  if (type === 'object') {
-    const res = {};
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        if (['object', 'array'].includes(getType(obj[key]))) {
-          res[key] = deepClone(obj[key]);
-        } else {
-          res[key] = obj[key];
-        }
-      }
-    }
-    return res;
-  } else if (type === 'array') {
-    const target = [];
-    for (const key in obj) {
-      if (['object', 'array'].includes(getType(obj[key]))) {
-        target.push(deepClone(obj[key]));
-      } else {
-        target.push(obj[key]);
-      }
-    }
+// 基本类型、对象、函数
+export const deepClone = (target: any, circleRef = new WeakMap()) => {
+  // 非引用类型
+  if (!isObject(target)) {
     return target;
+  }
+
+  const type = getType(target);
+
+  if (circleRef.has(target)) {
+    return circleRef.get(target);
+  }
+
+  // 引用类型处理
+  switch (type) {
+    case arrayTag:
+      // 数组
+      const initArr = new Array;
+      circleRef.set(target, initArr);
+      target.forEach((item) => {
+        initArr.push(deepClone(item, circleRef));
+      });
+      return initArr;
+    case mapTag:
+      // Map
+      const initMap = new Map();
+      circleRef.set(target, initMap);
+      for (const ele of target) {
+        initMap.set(deepClone(ele[0], circleRef), deepClone(ele[1], circleRef));
+      }
+      return initMap;
+    case setTag:
+      // Set
+      const initSet = new Set();
+      circleRef.set(target, initSet);
+      for (const ele of target) {
+        initSet.add(deepClone(ele, circleRef));
+      }
+      return initSet;
+    default:
+      const initObj = {};
+      circleRef.set(target, initObj);
+      for (const key in target) {
+        initObj[key] = deepClone(target[key], circleRef);
+      }
+      return initObj;
   }
 }
